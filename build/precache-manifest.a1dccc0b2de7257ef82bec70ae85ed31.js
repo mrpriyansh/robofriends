87dd82e03e7728e5fd7bd75c579173ecdbe059a3
Gaharana <mr.priyanshgaharana@gmail.com>
2020-05-19 07:18:35 +0530

    changed homepage

---
+++ build/precache-manifest.a1dccc0b2de7257ef82bec70ae85ed31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62002add94305561478312352ccc36db",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "a862e48426e9e746f8d4",
+    "url": "/robo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5c558df528c0bf54e67b",
+    "url": "/robo/static/css/main.25ba0aa9.chunk.css"
+  },
+  {
+    "revision": "a862e48426e9e746f8d4",
+    "url": "/robo/static/js/2.fab48d8d.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/robo/static/js/2.fab48d8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c558df528c0bf54e67b",
+    "url": "/robo/static/js/main.d4d3e1f1.chunk.js"
+  },
+  {
+    "revision": "641ff4b0b1bc6b4d6692",
+    "url": "/robo/static/js/runtime-main.1265e81f.js"
+  }
+]);
